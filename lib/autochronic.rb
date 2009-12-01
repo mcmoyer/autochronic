2ebfcdd78cf4c81d5f580a703cdcb072231aa168
Marlon Moyer <marlon@airband-66-226-203-5.airband.net>
2009-12-01 14:33:59 -0600

    patched time_zone_conversion method in Rails 2.3

--- lib/autochronic.rb
+++ lib/autochronic.rb
@@ -1,6 +1,25 @@
 require 'activerecord'
 require 'chronic'
 
+module ActiveRecord
+  module AttributeMethods
+    module ClassMethods
+      def define_write_method_for_time_zone_conversion(attr_name)
+        method_body = <<-EOV
+          def #{attr_name}=(time)
+            unless time.acts_like?(:time)
+              time = time.is_a?(String) ? (Chronic.parse(time) || Time.zone.parse(time)) : time.to_time rescue time
+            end
+            time = time.in_time_zone rescue nil if time
+            write_attribute(:#{attr_name}, time)
+          end
+        EOV
+        evaluate_attribute_method attr_name, method_body, "#{attr_name}="
+      end
+    end
+  end
+end
+
 class ActiveRecord::ConnectionAdapters::Column
   class << self
     def string_to_date_with_chronic(string)
